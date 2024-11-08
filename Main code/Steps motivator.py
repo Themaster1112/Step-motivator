fc6b9c880d2c5f7f56b0e14fd0750b3f3d6787f9
Pythonforbasics <114282587+Themaster1112@users.noreply.github.com>
2024-11-08 19:30:04 +0000

    Steps motivator project incase you need it.
    If you want to use it use it.

---
+++ Main code/Steps motivator.py
@@ -0,0 +1,6 @@
+#So for example you gave up in phisical movement well this is the thing for you
+Steps=input("How many steps did you today (You can use any watch to enter the steps?:")
+if Steps == "100" or "200" or "300":
+    print("Maybe do more excersize")
+else:
+    print("If you did less than 100 get those steps in if you did 300+ Good job :D")
